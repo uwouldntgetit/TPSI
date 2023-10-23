f40b4f0fedc83577ab533ff8714e8011f94d26b1
Federico <federicogiudici14@gmail.com>
2023-10-23 22:00:37 +0200

    fixed colorscheme change

--- es2/app.js
+++ es2/app.js
@@ -2,6 +2,9 @@ let textColor, bodyBGColor, containerBGColor;
 let title = document.getElementsByTagName("h1");
 const colorSchemeBtn = document.getElementById("clr-btn");
 const wrapper = document.getElementById("wrapper");
+const mainList = document.getElementsByClassName("main");
+const secondaryList = document.getElementsByClassName("secondary");
+
 
 function changeColorScheme(){
     console.log("ciao");
@@ -10,14 +13,34 @@ function changeColorScheme(){
     else
         colorSchemeBtn.innerHTML = "light_mode";
 
-    if(wrapper.classList.contains("colorscheme-light")){
-        wrapper.classList.add("colorscheme-dark");
-        wrapper.classList.remove("colorscheme-light");
+    // if(wrapper.classList.contains("colorscheme-light")){
+    //     wrapper.classList.add("colorscheme-dark");
+    //     wrapper.classList.remove("colorscheme-light");
+    // }
+    // else {
+    //     wrapper.classList.remove("colorscheme-dark");
+    //     wrapper.classList.add("colorscheme-light");
+    // }
+
+    for(const e of mainList){
+        if(e.classList.contains("colorscheme-light")){
+            e.classList.add("colorscheme-dark");
+            e.classList.remove("colorscheme-light");
+
+            for(const e of secondaryList){
+                e.classList.add("colorscheme-dark-sec")
+            }
+        }
+        else {
+            e.classList.remove("colorscheme-dark");
+            e.classList.add("colorscheme-light");
+
+            for(const e of secondaryList){
+                e.classList.remove("colorscheme-dark-sec")
+            }
+        }
     }
-    else {
-        wrapper.classList.remove("colorscheme-dark");
-        wrapper.classList.add("colorscheme-light");
-    }
+
         
 }
 
