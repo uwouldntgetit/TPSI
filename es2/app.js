e73b0112e5f6b5e56cc75238cf77f8832301cbad
Federico Giudici <federicogiudici10@gmail.com>
2023-10-23 20:26:34 +0200

    changes

--- es2/app.js
+++ es2/app.js
@@ -1,6 +1,7 @@
 let textColor, bodyBGColor, containerBGColor;
 let title = document.getElementsByTagName("h1");
 const colorSchemeBtn = document.getElementById("clr-btn");
+const wrapper = document.getElementById("wrapper");
 
 function changeColorScheme(){
     console.log("ciao");
@@ -9,6 +10,15 @@ function changeColorScheme(){
     else
         colorSchemeBtn.innerHTML = "light_mode";
 
+    if(wrapper.classList.contains("colorscheme-light")){
+        wrapper.classList.add("colorscheme-dark");
+        wrapper.classList.remove("colorscheme-light");
+    }
+    else {
+        wrapper.classList.remove("colorscheme-dark");
+        wrapper.classList.add("colorscheme-light");
+    }
         
 }
+
 
